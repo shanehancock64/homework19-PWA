edfe6ec37aca6f4a1a373a2bbab6eef4016a6117
Michael Hancock <shanehancock64@live.com>
2022-07-29 16:25:57 -0700

    Update install.js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -12,7 +12,16 @@ window.addEventListener('beforeinstallprompt', (event) => {
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
   const promptEvent = window.defferedPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  window.defferedPrompt = null;
+  butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+  window.defferedPrompt = null;
+});
