5a569a68ab976873a0dee8d534f3a127a60722fc
Michael Hancock <shanehancock64@live.com>
2022-07-29 16:17:43 -0700

    Update database.js
    put

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,15 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('updating jateDB');
+export const putDb = async (content) => {
+  console.error('updating jateDB');
+const jateDb = await openDB('jate', 1);
+const tx = jateDb.transaction('jate', 'readwrite');
+const objStore = tx.objectStore('jate');
+const req = objStore.put({ id: id, value: value })
+const res = await req;
+};
+  console.log('data saved to the jateDB', res);
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('Getting info from the jateDB');
+export const getDb = async () => {
+  console.error('Getting info from the jateDB');
 const jateDb = await openDB('jate', 1);
 const tx = jateDb.transaction('jate', 'readwrite');
 const objStore = tx.objectStore('jate');
 const req = objStore.getAll()
 const res = await req;
   console.log('data saved to the jateDB', res);
-
+}
 initdb();
