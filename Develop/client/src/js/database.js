733b1a76723895f4dd5430571dae0f9f16282aa5
Michael Hancock <shanehancock64@live.com>
2022-07-29 16:15:41 -0700

    Update database.js
    get method

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => console.error('updating jateDB');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error('Getting info from the jateDB');
+const jateDb = await openDB('jate', 1);
+const tx = jateDb.transaction('jate', 'readwrite');
+const objStore = tx.objectStore('jate');
+const req = objStore.getAll()
+const res = await req;
+  console.log('data saved to the jateDB', res);
 
 initdb();
